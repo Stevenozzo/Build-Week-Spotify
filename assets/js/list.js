cb075a43d1362343516657ee216fe67f2337c284
erikagarofalo <erika.garofalo@gmail.com>
2024-10-02 12:53:56 +0200

    new homepage

---
+++ assets/js/list.js
@@ -0,0 +1,14 @@
+const listaPlaylist = JSON.parse(localStorage.getItem("playlists") || []);
+const plist = document.getElementById("plist");
+
+listBuilder();
+
+function listBuilder() {
+  listaPlaylist.forEach((element) => {
+    plist.innerHTML += `
+        <li>
+            <a href="playlistsDetail.html?listId=${element.id}" class="text-decoration-none text-light">${element.namePlaylist}</a>
+        </li>
+        `;
+  });
+}
