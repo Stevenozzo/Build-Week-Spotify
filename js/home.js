b8c4bfb16e9a6afcf4222476e7554917697b73dd
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-10-02 11:11:41 +0200

    changed home js

--- js/home.js
+++ js/home.js
@@ -29,22 +29,49 @@ function searchArtistByName(artistName) {
       console.log("Errore nella ricerca dell'artista:", error);
     });
 }
+let albums = [];
+let albumTracks = [];
+const imgAlbum = document.getElementById("imgAlbum").src;
 
 function getArtistData(artistId) {
-  fetch(`https://api.spotify.com/v1/artists/${artistId}/top-tracks`, {
+  fetch(`https://api.spotify.com/v1/artists/${artistId}/albums`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
     },
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Dati dell'artista:", data);
+  .then((response) => response.json())
+  .then((data) => {
+    albums.push(data);
+    // console.log(albums[0].items[0].img[0].url);
+    // console.log(albums[0].items[0])
+    data.items.forEach((album) => {
+      let albums = album.id;
+      fetch(`https://api.spotify.com/v1/albums/${albums}/tracks`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((totBrani) => totBrani.json())
+      .then((brani)=> {
+        albumTracks.push(brani);
+        // console.log(brani);
+        
+      })
+      
     })
-    .catch((error) => {
-      console.log("Errore nella richiesta dell'artista:", error);
-    });
+    console.log("Dati dell'artista:", data);
+  })
+  .catch((error) => {
+    console.log("Errore nella richiesta dell'artista:", error);
+  });
 }
+
+console.log(albums);
+console.log(albumTracks);
+
+// console.log(albums[0].items[0])
 
 const searchBar = document.getElementById("search-bar");
 const searchButton = document.getElementById("search-button");
