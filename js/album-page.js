a461408b65fde6ff6304b21c4aaa513f41ec355c
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-10-04 09:44:56 +0200

    updated album.js

--- js/album-page.js
+++ js/album-page.js
@@ -1,4 +1,6 @@
-const token = localStorage.getItem("access_token");
+import { readCookie } from "./cookies.js";
+
+const token = readCookie("SpotifyBearer");
 if (!token) {
   location.href = "/index.html";
 }
@@ -149,7 +151,7 @@ function formatDuration(ms) {
 async function fetchArtistInfo(id) {
   try {
     const response = await fetch(`https://api.spotify.com/v1/artists/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` }
     });
     const artist = await response.json();
     return { name: artist.name, image: artist.images[2].url };
@@ -162,7 +164,7 @@ async function fetchArtistInfo(id) {
 async function fetchAlbumInfo(id) {
   try {
     const response = await fetch(`https://api.spotify.com/v1/albums/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` }
     });
     const album = await response.json();
     return {
@@ -170,16 +172,12 @@ async function fetchAlbumInfo(id) {
       artistName: album.artists[0].name,
       releaseDate: album.release_date,
       image: album.images[0].url,
-      tracks: album.tracks.items.map((track) => ({
+      tracks: album.tracks.items.map(track => ({
         name: track.name,
         duration: track.duration_ms,
         explicit: track.explicit,
-<<<<<<< HEAD
         uri: track.uri  // This is necessary for full playback
       }))
-=======
-      })),
->>>>>>> origin/Andrea
     };
   } catch (error) {
     console.error("Error fetching album info:", error);
@@ -189,9 +187,10 @@ async function fetchAlbumInfo(id) {
 
 async function fetchArtistPlaylists(artistName) {
   try {
-    const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=playlist&limit=10`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const response = await fetch(
+      `https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=playlist&limit=10`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
     const data = await response.json();
     return data.playlists.items;
   } catch (error) {
@@ -203,31 +202,22 @@ async function fetchArtistPlaylists(artistName) {
 function renderAlbumTracks(tracks, artistName) {
   const ulElement = document.getElementById("tracks-list");
   let totalDuration = 0;
-<<<<<<< HEAD
 
   let trackHtml = tracks.map((track, index) => {
     totalDuration += track.duration;
     return `
       <li class="flex space-between p-1 track-item" data-track-uri="${track.uri}">
-=======
-  let trackHtml = tracks
-    .map((track, index) => {
-      totalDuration += track.duration;
-      return `
-      <li class="flex space-between p-1">
->>>>>>> origin/Andrea
         <div class="flex width-30 align-center">
           <p class="flex p-1 align-center square-40 justify-end">${index + 1}</p>
           <div class="flex column justify-center">
             <p class="f-white">${track.name}</p> 
-            <p class="flex align-center">${track.explicit ? `<span class="explicit me-05">E</span>` : ""}${artistName}</p>
+            <p class="flex align-center">${track.explicit ? `<span class="explicit me-05">E</span>` : ''}${artistName}</p>
           </div>
         </div>
         <p class="width-30 flex align-center justify-end pe-1">${formatDuration(track.duration)}</p>
       </li>
     `;
-    })
-    .join("");
+  }).join("");
 
   ulElement.innerHTML = trackHtml;
 
@@ -257,10 +247,9 @@ function renderAlbumTracks(tracks, artistName) {
 function renderArtistPlaylists(playlists, artistName) {
   const headerElement = document.getElementById("playlist-header");
   headerElement.innerHTML = `<h4 class="p-1">Playlists for <br> ${artistName}</h4>`;
-  let playlistHtml = playlists
-    .map((playlist) => {
-      const playlistImage = playlist.images.length > 0 ? playlist.images[0].url : "default-image.jpg";
-      return `
+  let playlistHtml = playlists.map(playlist => {
+    const playlistImage = playlist.images.length > 0 ? playlist.images[0].url : 'default-image.jpg';
+    return `
       <li class="flex space-between py-05">
         <div class="flex align-center ps-1" style="color:#B3B3B3">
           <img src="${playlistImage}" class="square-50" alt="Playlist Image">
@@ -272,14 +261,16 @@ function renderArtistPlaylists(playlists, artistName) {
         <p class="flex align-center text-end pe-1 justify-end width-30" style="color:#B3B3B3">${playlist.tracks.total} tracks</p>
       </li>
     `;
-    })
-    .join("");
+  }).join("");
 
   headerElement.innerHTML += playlistHtml;
 }
 
 async function populatePage() {
-  const [albumInfo, artistInfo] = await Promise.all([fetchAlbumInfo(albumId), fetchArtistInfo(artistId)]);
+  const [albumInfo, artistInfo] = await Promise.all([
+    fetchAlbumInfo(albumId),
+    fetchArtistInfo(artistId)
+  ]);
 
   document.getElementById("album-image").src = albumInfo.image;
   document.getElementById("artist-name").innerText = artistInfo.name;
@@ -295,11 +286,11 @@ async function populatePage() {
     renderArtistPlaylists(artistPlaylists, artistInfo.name);
   }
 
-  import("./bg-gradient.js")
-    .then(() => console.log("bg-gradient.js has been loaded"))
-    .catch((error) => console.error("Error loading bg-gradient.js:", error));
+  import('./bg-gradient.js')
+    .then(() => console.log('bg-gradient.js has been loaded'))
+    .catch((error) => console.error('Error loading bg-gradient.js:', error));
 
-  document.querySelector("main").classList.remove("hidden");
+  document.querySelector('main').classList.remove("hidden");
 }
 
 populatePage();
